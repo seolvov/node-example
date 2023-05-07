c23592977c0d6395a14a8dcd14954e4515fb20f5
seol <hhhseolah@gmail.com>
2023-05-07 23:45:56 +0900

    get post put delete test

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,26 @@ import express from "express";
 
 const router = express.Router()
 
-console.log("success")
+// console.log("success")
+router.get("/all", (req, res) => {
+    res.json({
+        msg: "product get all"
+    })
+})
+router.post("/create", (req, res) => {
+    res.json({
+        msg: "created a product"
+    })
+})
+router.put("/update", (req, res) => {
+    res.json({
+        msg: "updated a product"
+    })
+})
+router.delete("/delete", (req, res) => {
+    res.json({
+        msg: "deleted a product"
+    })
+})
 
 export default router
