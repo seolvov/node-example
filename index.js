2bb048387aaecf93116f526d18d7caf7043699a5
seol <hhhseolah@gmail.com>
2023-05-07 23:31:47 +0900

    add morgan, cors, nodemon

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express"
+import cors from "cors"
+import morgan from "morgan"
 
 const app = express()
 const port = 8080
 
+app.use(cors())
+app.use(morgan('dev'))
 app.get("/test", (req, res) => {
     res.json({
         msg: "test api"
